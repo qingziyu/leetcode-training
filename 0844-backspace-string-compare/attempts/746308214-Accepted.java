e0d98367b2ae353428d6ca7f1c40e13574c08038
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-02 00:22:41 -0700

    Archive Accepted attempt 746308214: 0844-backspace-string-compare

---
+++ 0844-backspace-string-compare/attempts/746308214-Accepted.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Deque<Character> stack = new ArrayDeque<>();
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if (stack.size() > 0 && c == '#') {
+                stack.remove();
+            } else if (c != '#') {
+                stack.push(c);
+            }
+        }
+
+        for (int i = t.length() - 1; i >= 0; i--) {
+            char currentChar = t.charAt(i);
+            if (currentChar == '#') {
+                count++;
+            } else if (currentChar != '#' && count > 0) {
+                count--;
+                continue;
+            } else if (stack.size() > 0 && currentChar == stack.peek()) {
+                stack.remove();       
+            } else {
+                return false;
+            }
+        }
+
+        if (stack.size() > 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
