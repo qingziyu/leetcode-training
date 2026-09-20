0b874c5013b476e9870cb974ee582b76388a650b
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-20 02:15:42 -0700

    Archive Wrong Answer attempt 750429183: 0020-valid-parentheses

---
+++ 0020-valid-parentheses/attempts/750429183-Wrong-Answer.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
+                stack.push(currentChar);
+            } 
+            
+            if (currentChar == ')') {
+                if (stack.peek() == '('){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+
+            if (currentChar == ']') {
+                if (stack.peek() == '['){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+
+            if (currentChar == '}') {
+                if (stack.peek() == '{'){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
