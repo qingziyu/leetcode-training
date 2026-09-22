8e5a4f8b41470aab66694d1d04d16818ebfe0305
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-22 00:53:54 -0700

    Archive Wrong Answer attempt 750865859: 0080-remove-duplicates-from-sorted-array-ii

---
+++ 0080-remove-duplicates-from-sorted-array-ii/attempts/750865859-Wrong-Answer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int writePointer = 1;
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            int currentNum = nums[i];
+            int previousNum = nums[i - 1];
+
+            if (previousNum == currentNum) {
+                count++;
+                if (count <= 2) {
+                    writePointer++;
+                    continue;
+                } else {
+                    count++;
+                    continue;
+                }
+            } else {
+                nums[writePointer] = currentNum;
+                writePointer++;
+                count = 1;
+            }
+        }
+
+        return writePointer;
+    }
+}
