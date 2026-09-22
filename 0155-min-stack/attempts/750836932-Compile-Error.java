44f159a4a385030e07c9c2996387866711164051
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-21 23:19:45 -0700

    Archive Compile Error attempt 750836932: 0155-min-stack

---
+++ 0155-min-stack/attempts/750836932-Compile-Error.java
@@ -0,0 +1,39 @@
+class MinStack {
+    int currentInt;
+    MinStack previousStack;
+    int minSeenInt;
+
+    public MinStack(int currentInt, MinStack previousStack) {
+        this.currentInt = currentInt;
+        this.previousStack = previousStack;
+
+        if (currentInt < minSeenInt) {
+            minSeenInt = currentInt;
+        }
+    }
+    
+    public void push(int value) {
+        MinStack newMinStack = new MinStack(value, this);
+    }
+    
+    public void pop() {
+        this = this.previousStack;
+    }
+    
+    public int top() {
+        return currentInt;
+    }
+    
+    public int getMin() {
+        return minSeenInt;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
