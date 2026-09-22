46ba96bf5ce175f02576b9aed80135433edc2fda
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-21 23:05:20 -0700

    Archive Wrong Answer attempt 750833827: 0155-min-stack

---
+++ 0155-min-stack/attempts/750833827-Wrong-Answer.java
@@ -0,0 +1,36 @@
+class MinStack {
+    int currentInt;
+    MinStack previousStack;
+    int minSeenInt;
+
+    public MinStack() {
+        int currentInt;
+        MinStack previousStack;
+        int minSeenInt;
+    }
+    
+    public void push(int value) {
+
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        return currentInt;
+    }
+    
+    public int getMin() {
+        return minSeenInt;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
