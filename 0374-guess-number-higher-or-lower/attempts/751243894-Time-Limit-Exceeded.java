e39a1ca3578d13f9a76e96fa66e82c16e96e3603
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-23 22:45:40 -0700

    Archive Time Limit Exceeded attempt 751243894: 0374-guess-number-higher-or-lower

---
+++ 0374-guess-number-higher-or-lower/attempts/751243894-Time-Limit-Exceeded.java
@@ -0,0 +1,29 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int leftPointer = 0;
+        int rightPointer = n;
+        int resultNum = (leftPointer + rightPointer)/2;
+
+        while(true) {
+            resultNum = (leftPointer + rightPointer)/2;
+            int result = guess(resultNum);
+
+            if (result == 0) {
+                return resultNum;
+            } if (result == -1) {
+                rightPointer = resultNum - 1;
+            } else {
+                leftPointer = leftPointer + 1;
+            }
+        }
+    }
+}
