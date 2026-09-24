a8a9c48dc511da57f5882653193b9d8db1e1076a
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-23 22:25:11 -0700

    Time: 0 ms (100%), Space: 47.7 MB (28.91%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int leftPointer = 0;
+        int rightPointer = nums.length - 1;
+        int midPointer = (leftPointer + rightPointer) / 2;
+
+        while (leftPointer <= rightPointer) {
+            int currentNum = nums[midPointer];
+
+            if (currentNum == target) {
+                return midPointer;
+            } else if (currentNum > target) {
+                rightPointer = midPointer - 1;
+            } else {
+                leftPointer = midPointer + 1;   
+            }
+
+            midPointer = (leftPointer + rightPointer) / 2;
+        }
+
+        return -1;
+    }
+}
