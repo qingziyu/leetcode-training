9bd79086331e68d2ac2462485189b029defebc10
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-24 20:26:07 -0700

    Archive Compile Error attempt 751365023: 0682-baseball-game

---
+++ 0682-baseball-game/attempts/751365023-Compile-Error.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int result = 0;
+
+        for (int i = 0; i < operations.length; i++) {
+            String currentStr = operations[i];
+            if (currentStr.matches("[0-9]+")) {
+                stack.add(Integer.parseInt(currentStr));
+            } else if (currentStr == "C") {
+                stack.pop();
+            } else if (currentStr == "D") {
+                int topInt = Integer.parseInt(currentStr);
+                stack.add(topInt*2);
+            } else {
+                int topInt = Integer.parseInt(currentStr);
+                stack.pop();
+                int secInt = Integer.parseInt(stack.peek());
+
+                stack.add(secInt);
+                stack.add(topInt);
+                stack.add(secInt + topInt);
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+}
