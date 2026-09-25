1d8f9c1519e18de0228d7abf280abb21e75f3552
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-24 19:19:25 -0700

    Archive Wrong Answer attempt 751357622: 0035-search-insert-position

---
+++ 0035-search-insert-position/attempts/751357622-Wrong-Answer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int leftPointer = 0;
+        int rightPointer = nums.length - 1;
+        int midPoiner = (leftPointer + rightPointer)/2;;
+
+        while(leftPointer < rightPointer) {
+            int currentMid = nums[midPoiner];
+
+            if (currentMid == target) {
+                return midPoiner;
+            } else if (currentMid < target) {
+                leftPointer = midPoiner + 1;
+            } else {
+                rightPointer = midPoiner - 1;
+            }
+
+            midPoiner = (leftPointer + rightPointer)/2;
+
+            currentMid = nums[midPoiner];
+            if (rightPointer - leftPointer == 1 && currentMid != target) {
+                return leftPointer + 1;
+            }
+        }
+
+        return midPoiner;
+    }
+}
