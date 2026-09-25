8b180f422cdb03e0ced790d4859c152ef9449d3d
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-24 21:57:19 -0700

    Archive Time Limit Exceeded attempt 751372879: 0069-sqrtx

---
+++ 0069-sqrtx/attempts/751372879-Time-Limit-Exceeded.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        int leftPointer = 1;
+        int rightPointer = x - 1;
+
+        while(leftPointer <= rightPointer) {
+            int mid = (rightPointer - leftPointer)/2 + leftPointer;
+            int result = mid*mid;
+
+            if(result > x) {
+                rightPointer = mid - 1;
+            } else if (result < x) {
+                leftPointer = mid;
+            } else {
+                return mid;
+            }
+        }
+
+        return rightPointer;
+    }
+}
