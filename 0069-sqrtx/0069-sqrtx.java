8b17632b1c973ae8e2118612d9fe383d675084a4
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-24 22:06:13 -0700

    Time: 1 ms (99.4%), Space: 41.9 MB (70.14%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        long leftPointer = 0;
+        long rightPointer = x;
+
+        while(leftPointer < rightPointer) {
+            long mid = (rightPointer - leftPointer + 1)/2 + leftPointer;
+            long result = mid*mid;
+
+            if(result > x) {
+                rightPointer = mid - 1;
+            } else if (result < x) {
+                leftPointer = mid;
+            } else {
+                return (int) mid;
+            }
+        }
+
+        return (int) rightPointer;
+    }
+}
