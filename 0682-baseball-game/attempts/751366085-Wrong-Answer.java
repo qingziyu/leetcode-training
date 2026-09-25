0435c09a7b54a4ae62e4c5639ea2d2ccf1461d28
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-24 20:36:13 -0700

    Archive Wrong Answer attempt 751366085: 0682-baseball-game

---
+++ 0682-baseball-game/attempts/751366085-Wrong-Answer.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int result = 0;
+
+        for (int i = 0; i < operations.length; i++) {
+            String currentStr = operations[i];
+            if (currentStr.matches("-?[0-9]+")) {
+                stack.add(Integer.parseInt(currentStr));
+            } else if (currentStr.equals("C")) {
+                stack.pop();
+            } else if (currentStr.equals("D")) {
+                int currentNum = stack.peek();
+                stack.add(currentNum*2);
+            } else {
+                int topInt = stack.pop();
+                int secInt = stack.peek();
+
+                stack.add(topInt);
+                stack.add(secInt + topInt);
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            int topNum = stack.pop();
+            result = topNum + result;
+        }
+
+        return result;
+    }
+}
