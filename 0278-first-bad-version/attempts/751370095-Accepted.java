58c985853f7dfed2fd9f2334e3bf2764d00e1db4
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-24 21:19:35 -0700

    Archive Accepted attempt 751370095: 0278-first-bad-version

---
+++ 0278-first-bad-version/attempts/751370095-Accepted.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int leftPointer = 1;
+        int rightPointer = n;
+
+        while(leftPointer < rightPointer) {
+            int midPointer = (rightPointer - leftPointer)/2 + leftPointer;
+
+            if (isBadVersion(midPointer)) {
+                rightPointer = midPointer;
+            } else {
+                leftPointer = midPointer + 1;
+            }
+        }
+
+        return leftPointer;
+    }
+}
