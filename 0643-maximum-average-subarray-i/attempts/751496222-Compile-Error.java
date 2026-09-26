02bbe33694ef05890fd18c9cceeec2e934e3d7c7
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-25 22:59:21 -0700

    Archive Compile Error attempt 751496222: 0643-maximum-average-subarray-i

---
+++ 0643-maximum-average-subarray-i/attempts/751496222-Compile-Error.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int leftPointer = 0;
+        int rightPointer = k - 1;
+        int result = 0;
+
+        for (int i = 0; i <= rightPointer; i++) {
+            result += nums[i];
+        }
+
+        while(rightPointer < nums.length - 1) {
+            int previousLeft = nums[leftPointer];
+            int currentRight = nums[rightPointer + 1];
+            int currentResult = currentResult - previousLeft + currentRight;
+
+            if (currentResult > result) {
+                result = currentResult;
+            }
+
+            leftPointer++;
+            rightPointer++;
+        }
+
+        return (double) result/k;
+    }
+}
