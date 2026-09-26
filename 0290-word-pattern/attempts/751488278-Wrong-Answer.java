d315f88cb992bc15be42d7058826e9fb6da881de
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-25 21:40:45 -0700

    Archive Wrong Answer attempt 751488278: 0290-word-pattern

---
+++ 0290-word-pattern/attempts/751488278-Wrong-Answer.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> patternMap = new HashMap<Character, String>();
+        int sPointer = 0;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char currentChar = pattern.charAt(i);
+            int nextSpaceIndex = findNextSpace(s, sPointer);
+            String sString = s.substring(sPointer, nextSpaceIndex);
+
+            if (patternMap.containsKey(currentChar)) {
+                if (!patternMap.get(currentChar).equals(sString)) {
+                    return false;
+                }
+            } else {
+                patternMap.put(currentChar, sString);
+            }
+        }
+
+        return true;
+    }
+
+    private int findNextSpace(String s, int pointer) {
+        while(true) {
+            pointer++;
+            if (s.charAt(pointer) == ' ') {
+                return pointer;
+            }
+        }
+    }
+}
