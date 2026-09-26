d5df460b4d2cee39e3195440c893612730be9e23
qingziyu <49860793+qingziyu@users.noreply.github.com>
2026-09-25 21:19:10 -0700

    Time: 34 ms (16.96%), Space: 102.6 MB (60.99%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> seenNums = new HashSet<>();
+
+        if (k == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentNum = nums[i];
+
+            if(i == 0) {
+                seenNums.add(currentNum);
+                continue;
+            }
+
+            if (seenNums.contains(currentNum)) {
+                return true;
+            } else {
+                seenNums.add(currentNum);
+            }
+
+            if (i >= k) {
+                int removeInt = nums[i - k];
+                seenNums.remove(removeInt);
+            }
+        }
+
+        return false;
+    }
+}
